abbfd733426cfc3a939055a5af48ed6257cc011e
tmstafford <tatumstafford1@gmail.com>
2020-11-30 13:04:31 -0600

    inquirer prompts functions for creating new members

--- index.js
+++ index.js
@@ -34,11 +34,84 @@ function promptManager() {
     ])
     .then(function({ name, id, email, officeNumber}) {
         const manager = new Manager(name, id, email, officeNumber);
-        const role = manager.getRole();
-        console.log(role);
-        console.log(manager);
-
+        teamArray.push(manager);
+        console.log(teamArray);
+        addMember();
     })
 };
+
+function addMember() {
+    return inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'newMember',
+            message: 'Would you like to add another team member?'
+        }
+    ])
+    .then(function({ newMember }) {
+        if (newMember) {
+            memberInfo();
+        } else {
+            // function to end HTML
+        }
+    })
+};
+
+function memberInfo() {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: "Select the team member's role:",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the team member's name?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Enter the team member's ID:"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Enter the team member's email address:"
+        }
+    ])
+    .then(function({ role, name, id, email }) {
+        if (role === 'Engineer') {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'github',
+                    message: "Enter the team member's GitHub Username:"
+                }
+            ])
+            .then(function({ github }) {
+                const engineer = new Engineer(name, id, email, github);
+                teamArray.push(engineer);
+                console.log(teamArray);
+                addMember();
+            })
+        } else if (role === 'Intern') {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'school',
+                    message: "Enter the team member's school name:"
+                }
+            ])
+            .then(function({ school }) {
+                const intern = new Intern(name, id, email, school);
+                teamArray.push(intern);
+                console.log(teamArray);
+                addMember();
+            })
+        }
+    })
+}
 
 promptManager();
